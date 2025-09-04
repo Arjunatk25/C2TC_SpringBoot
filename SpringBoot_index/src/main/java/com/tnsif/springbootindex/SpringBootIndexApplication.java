a0030f97dc507282debf3d668ddd9557ef58a98c
Arjunatk25 <144626188+Arjunatk25@users.noreply.github.com>
2025-09-04 22:36:36 +0530

    "done"

---
+++ SpringBoot_index/src/main/java/com/tnsif/springbootindex/SpringBootIndexApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.springbootindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIndexApplication.class, args);
+	}
+
+}
